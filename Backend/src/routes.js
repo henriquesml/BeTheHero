e6bb18d89bc9bf0ea60d5deb4061706d4b8f204b
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-03-27 21:23:35 -0300

    criando e listando ongs

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+const OngController = require('./controller/OngController')
+
 const routes = express.Router()
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
 module.exports = routes
